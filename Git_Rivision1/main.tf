a4b9da8bd427070506e31248509df024e834c768
SN262024 <surendrachauhan18@gmail.com>
2025-08-22 23:20:55 +0530

    added code

---
+++ Git_Rivision1/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-Nikasi"
+    storage_account_name = "stgpk"
+    container_name       = "containerpk"
+    key                  = "prod.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "177b7e12-5f03-4f63-bcd1-ed6d1d776bff"
+}
+
+resource "azurerm_resource_group" "rg_pk" {
+  name     = "rg-pk"
+  location = "West Europe"
+}
+resource "azurerm_storage_account" "stgpk1" {
+  name                     = "storageaccountpk"
+  resource_group_name      = azurerm_resource_group.rg_pk.name
+  location                 = azurerm_resource_group.rg_pk.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
